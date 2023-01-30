6a45af1f70411a3726d15ca01bc1f29b7ed61e03
palminski <williambolls98@gmail.com>
2023-01-30 09:29:35 -0800

    setting up test js to use on SWISS

---
+++ test-javascript.js
@@ -0,0 +1,15 @@
+id="Swis-Referral-ReferralTypeField-input"
+
+document.getElementById("Swis-Referral-ReferralTypeField-input").value = "Major";
+document.getElementById("Swis-Referral-StudentField-input").value = "Test";
+document.getElementById("Swis-Referral-GradeField-input").value = "8";
+document.getElementById("Swis-Referral-EducatorField-input").value = "Test Name";
+document.getElementById("Swis-Referral-DateField-input").value = "10/1/23";
+document.getElementById("Swis-Referral-LocationField-input").value = "8:00 AM";
+document.getElementById("Swis-Referral-ProblemBehaviorField-0-input").value = "Being Naughty";
+document.getElementById("Swis-Referral-MotivationField-input").value = "Because";
+document.getElementById("Swis-Referral-OthersInvolvedField-input").value = "None";
+document.getElementById("Swis-Referral-ActionTakenField-0-input").value = "Restitution/Community Service";
+document.getElementById("Swis-Referral-ActionTakenField-1-input").value = "Stockade";
+document.getElementById("Swis-Referral-NotesField-input").value = "None";
+document.getElementById("Swis-Referral-CustomField-90331-input").value = "No"; //hate Symbol
